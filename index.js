54752ccf451daeddd47028ca9a0e23f38d4d3c6f
Patrick-source-web <patrick20191995@gmail.com>
2025-11-07 18:37:47 +0200

    calculate a value

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-  //Objects // Non primitive data type
+        //Objects // Non primitive data type
         // console.log("JS file loaded!");
 
         // let person = {
@@ -21,11 +21,23 @@
         // console.log(fruits)
 
         // Functions
-        function yearOfBirth(age) {
-            console.log(age)
-        }
-        yearOfBirth(2022)
+        // function old (age) {
+        //     // console.log("My name is Patrick and I'm " + " " + age + " " + "years old" )
+        // }
+        // old(20)
         function greet (name, lastName){
             console.log("Hello" + " " + name + " " + lastName)
+            console.log("Hello, My name is" + " " + name + " " + lastName )
         }
         greet("Patrick", "Iradukunda");
+
+        //Calculate a value using a function and retrun keyword//
+
+        function calculateAge (age1){
+            let age2= 1995
+            return(age1-age2);
+        }
+
+        console.log(calculateAge(2002));
+
+
